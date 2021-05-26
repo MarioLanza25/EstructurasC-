e6792e1f8f58dd8bb8e27e9fe0f0a58de3ba21f5
Mario Toruño <80336294+MarioLanza25@users.noreply.github.com>
2021-05-26 08:26:19 -0600

    FrmAdded

---
+++ EstructurasControl/EstructurasControl/FrmInterative.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace EstructurasControl
+{
+    partial class FrmInterative
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(100, 43);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(204, 20);
+            this.txtNumeros.TabIndex = 0;
+            this.txtNumeros.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TextBox1_KeyDown);
+            this.txtNumeros.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(35, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numeros";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(100, 83);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(204, 141);
+            this.txtResultado.TabIndex = 2;
+            this.txtResultado.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TextBox1_KeyPress);
+            // 
+            // FrmInterative
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(413, 236);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtNumeros);
+            this.Name = "FrmInterative";
+            this.Text = "FrmInterative";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeros;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtResultado;
+    }
+}
